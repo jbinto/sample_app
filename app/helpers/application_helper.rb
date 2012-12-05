7e3a1ca0bd8a38d032200be4f9341c9b626298f4
Jesse Buchanan <jbinto@jbinto.ca>
2012-12-05 12:43:08 -0600

    add full_title helper, fix missing require in Guardfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(title)
+    base = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      base
+    else
+      "#{base} | #{title}"
+    end
+  end
 end
