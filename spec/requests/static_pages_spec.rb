42215febc80649e0b9c923e8cc7ceadf96e09bd4
Jesse Buchanan <jbinto@jbinto.ca>
2012-12-07 12:10:51 -0500

    refactor spec to use `before` block

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,32 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  before { visit root_path }
 
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the base title" do
-      visit root_path
       page.should have_selector('title', 
           :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom title" do
-      visit root_path
       page.should_not have_selector('title', :text => "| Home")
     end
   end
   
   describe "Help page" do
+    before { visit help_path } 
+
     it "should have the content 'Help'" do
-      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the correct title" do
-      visit help_path
       page.should have_selector('title', 
           :text => "#{base_title} | Help")
     end
@@ -37,26 +34,26 @@ describe "Static pages" do
   end
 
   describe "About page" do
+    before { visit about_path }
+
     it "should have the content 'About Us'" do
-      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the correct title" do
-      visit about_path
       page.should have_selector('title', 
           :text => "#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
+
     it "should have the content 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the correct title" do
-      visit contact_path
       page.should have_selector('title', 
           :text => "#{base_title} | Contact")
     end
