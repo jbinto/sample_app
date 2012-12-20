387395f3d5aeb8d02f2a9d8a729db4a0eaec83ea
Jesse Buchanan <jbinto@jbinto.ca>
2012-12-20 17:10:35 -0500

    Sign in page: initial revision.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if (user && user.authenticate(params[:session][:password]))  # tricky; because it returns false if wrong pw
+      # Sign in the user.
+      session[:user] = user
+      redirect_to user 
+    else
+      flash.now[:error] = "Invalid username/password."
+      render 'new'
+    end
   end
 
   def destroy
